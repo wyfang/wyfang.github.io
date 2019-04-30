c4af822578b935e00d5ef8c4962c3c5282a105eb
Player <wyfang@qq.com>
2019-04-30 14:58:18 +0800

    Update

---
+++ img/lottie/luckincoffee.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('luckincoffee'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'img/lottie/luckincoffee.json'
+})
