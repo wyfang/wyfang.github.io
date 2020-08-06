8a1e6e63e18fbbac385dc14175a8226efb971f41
Player <wyfang@qq.com>
2020-08-06 16:58:55 +0800

    Update wifiwebsitetime.js

--- wifiwebsitetime.js
+++ wifiwebsitetime.js
@@ -44,3 +44,15 @@ function setTime() {
 }
 setTime();
 setInterval(setTime, 1000);
+
+function countDown() {
+    var nowtime = +new Date();
+    var inputTime = new Date('2112,08,27 11:13:32');
+    var times = (inputTime - nowtime) / 1000; //秒
+    var y = parseInt(times / 60 / 60 / 24 / 365);
+    y = y < 10 ? '0' + y : y;
+
+    return y + '年';
+}
+var years = document.getElementById('htmer_time2');
+years.innerText = countDown();
