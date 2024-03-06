ebb51d876204997177cb12b703b6b4fad5f9d6bd
Player <wyfang@qq.com>
2024-03-06 20:41:55 +0800

    Update wifitimecss.js

--- wifitimecss.js
+++ wifitimecss.js
@@ -1,21 +1,14 @@
-// window.onload = function () {
-// 	var wifitimecss = new Date().getHours();
-// 	if (wifitimecss > 6 && wifitimecss < 18) {
-// 		document.getElementById("wifitimecss").setAttribute("href", "/index.css?t=" + Date.now())
-// 	} else {
-// 		document.getElementById("wifitimecss").setAttribute("href", "/index-night.css")
-// 	}
-// }
-
 window.onload = function () {
     var wifitimecss = new Date().getHours();
     var themeColorMeta = document.querySelector('meta[name="theme-color"]');
-
+    
     if (wifitimecss > 6 && wifitimecss < 18) {
-        document.getElementById("wifitimecss").setAttribute("href", "/index.css?t=" + Date.now());
-        themeColorMeta.setAttribute("content", "#FFFFFF"); // 例如：#FFFFFF
+        document.getElementById("wifitimecss").setAttribute("href", "/index.css");
+        themeColorMeta.setAttribute("content", "#ECF1F3");
     } else {
-        document.getElementById("wifitimecss").setAttribute("href", "/index-night.css");
-        themeColorMeta.setAttribute("content", "#000000"); // 例如：#000000
+        document.getElementById("wifitimecss").setAttribute("href", "/index-night.css?t=" + Date.now());
+        setTimeout(function() {
+            themeColorMeta.setAttribute("content", "#080C0F");
+        }, 1000);
     }
 }
